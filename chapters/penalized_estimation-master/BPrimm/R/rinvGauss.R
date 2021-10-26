810abd99900aa41d5382efc73cd2d1dc6077c653
Ahmad <ahmad.ehyaei@gmail.com>
2021-10-26 17:01:31 +0200

    error

--- chapters/penalized_estimation-master/BPrimm/R/rinvGauss.R
+++
@@ -1,9 +0,0 @@
-rinvGauss <- function (n, mu, lambda){
-
-  x = numeric(n)
-
-  Z = .C("rinvGauss", x=as.double(x), as.integer(n),
-     as.double(mu), as.double(lambda), PACKAGE="BPrimm")
-
-  Z[["x"]]
-}
--- chapters/penalized_estimation-master/BPrimm/src/optim.h
+++
@@ -1,4 +0,0 @@
-double fn(int n, double *x, void *ex);
-
-void solve_bj(int* dim, double *xin, double *x, double *Fmin, 
-	int *fail, double* control, int *fncount, double *ex);
